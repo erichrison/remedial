4b3fa2e08b6a3858e80e80a3abfa87384b5d995d
Chris Erickson <chris.erickson@gmail.com>
2012-11-18 22:08:10 -0800

    Wrote enough custom vector implementation for appending and sorting.

--- week1/src/rtlvector/rtlvector.go
+++ week1/src/rtlvector/rtlvector.go
@@ -1,11 +1,56 @@
 package rtlvector
 
+import (
+    "fmt"
+    "strings"
+)
+
+type Elem interface {}
+
 type Vector struct {
     length int
+    data []Elem
+    cmp func(a, b Elem) bool
 }
 
-const growth_multiplier = 2
+func NewVector() Vector {
+    return NewSortableVector(nil)
+}
 
-func (v *Vector) Len() int {
+func NewSortableVector(cmp func(a, b Elem) bool) Vector {
+    data := make([]Elem, 10, 10)
+    return Vector{0, data, cmp}
+}
+
+func (v Vector) String() string {
+    ss := make([]string, v.length)
+    for i := 0; i < v.length; i++ {
+        ss[i] = fmt.Sprint(v.data[i])
+    }
+    return "[" + strings.Join(ss, ", ") + "]"
+}
+
+func (v Vector) Length() int {
     return v.length
+}
+
+func (v Vector) Swap(i, j int) {
+    v.data[i], v.data[j] = v.data[j], v.data[i]
+}
+
+func (v Vector) Compare(i, j int) bool {
+    return v.cmp(v.data[i], v.data[j])
+}
+
+func (v Vector) PushBack(val interface{}) Vector {
+    if v.length == cap(v.data) {
+        olddata := v.data
+        v.data = make([]Elem, 2 * cap(olddata), 2 * cap(olddata))
+        for i := 0; i < v.length; i++ {
+            v.data[i] = olddata[i]
+        }
+    }
+    v.data[v.length] = val
+    v.length++
+    return v
 }
--- week1/src/week1/week1.go
+++ week1/src/week1/week1.go
@@ -8,27 +8,8 @@ import (
     "strings"
 
     "rtlsort"
-    //"rtlvector"
+    "rtlvector"
 )
-
-type SimpleArray struct {
-    data []int
-}
-
-func (a SimpleArray) Length() int {
-    return len(a.data)
-}
-
-func (a SimpleArray) Compare(i, j int) bool {
-    return a.data[i] < a.data[j]
-}
-
-func (a SimpleArray) Swap(i, j int) {
-    a.data[i], a.data[j] = a.data[j], a.data[i]
-    //t := a.data[i]
-    //a.data[i] = a.data[j]
-    //a.data[j] = t
-}
 
 func main() {
     for i, path := range os.Args {
@@ -37,18 +18,16 @@ func main() {
         }
         text, err := ioutil.ReadFile(path)
         if err != nil { panic(err) }
-        var vec []int = nil
-        //vec := rtlvector.NewVector()
+        vec := rtlvector.NewSortableVector(func(a, b rtlvector.Elem) bool { return a.(int) < b.(int) })
         for _, s := range strings.Split(string(text), ",") {
-            //vec.PushBack(strings.TrimSpace(s))
             n, err := strconv.Atoi(strings.TrimSpace(s))
             if err == nil {
-                vec = append(vec, n)
+                vec = vec.PushBack(n)
             } else {
                 fmt.Println("Non-integer: " + strings.TrimSpace(s))
             }
         }
-        rtlsort.QuickSort(SimpleArray{vec})
+        rtlsort.QuickSort(vec)
         fmt.Println(vec)
     }
 }
